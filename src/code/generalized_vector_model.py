4e7042f902b1926a7c814bd1cf14b082bb5b32d3
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2024-03-05 04:42:00 -0500

    modelo vectorial generalizado completo

--- src/code/generalized_vector_model.py
+++ src/code/generalized_vector_model.py
@@ -1,27 +1,18 @@
 import itertools
 import json
+import numpy as np
+from math import log
+from preprocessing_query import *
 
-''' def generate_minterms(processed_query):
-    with open("corpus.json", "r") as archive: #asumiendo que el json con el corpus se llama corpus
-        data = json.load(archive)
-    corpus = data["data"["corpus"]] #asumiendo que el dicc se llame data y que la lista del corpus se llame corpus
-    vector_rep = data["data"["vector_representation"]] #asumiendo que la lista de pesos se llame vector_representation
+with open("corpus.json", "r") as json_file:
+    data = json.load(json_file)
 
-    #generar todas las combinaciones de literales
-    minterms = []
+filtered_docs = data["corpus"]
+weight_doc_matrix = data["vector representation"]
+vocabulary = data["vocabulary"]
+correlation_matrix = data["correlation_matrix"]
 
-    for i in range(1, len(processed_query) + 1):
-        for comb in itertools.combinations(processed_query, i):
-            if len(comb) == 1:
-                minterms.append(comb[0])
-            else:
-                minterm = comb[0]
-                for literal in comb[1:]:
-                    minterm += " AND " + literal
-                minterms.append(minterm)
-                '''
-
-def generate_minterm_of_document(doc, processed_query):
+'''def generate_minterm_of_document(doc, processed_query):
     minterms = []
 
     for term in processed_query:
@@ -29,4 +20,45 @@ def generate_minterm_of_document(doc, processed_query):
             minterms.append(1)
         else:
             minterms.append(0)
-    return minterms
+    return minterms'''
+
+def calculate_similarity(doc_index, processed_query):
+    n = len(correlation_matrix[0])
+    numerator = 0
+    sum_den_i_k = 0
+    sum_den_i_q = 0
+    weight_query_vector = get_weight_query_vector(processed_query)
+
+    for j in range(0,n):
+        for i in range[0:n]:
+            w_ik = get_weight_doc(doc_index,i)
+            w_jk = get_weight_doc(doc_index, j)
+            w_iq = weight_query_vector(i)
+            t_i_j = correlation_matrix[i][j]
+            numerator = numerator + (w_ik * w_jk * t_i_j)
+            sum_den_i_k = sum_den_i_k + (w_ik ** 2)
+            sum_den_i_q = sum_den_i_q + (w_iq ** 2)
+    
+    denominator = np.sqrt(sum_den_i_k) * np.sqrt(sum_den_i_q)
+
+    return numerator / denominator
+
+# TODO: Revisar si es ese el orden de la matriz de pesos
+def get_weight_doc(doc_index, term_index):
+    return weight_doc_matrix[doc_index][term_index]
+
+def weight_for_query(term, processed_query):
+    #Calcular TF
+    term_count = processed_query.count(term)
+    total_terms = len(processed_query)
+    tf = term_count/total_terms
+    exp = 1
+    #Calcular IDF
+    idf = log(exp, 10) + 1
+    return tf * idf
+
+def get_weight_query_vector(processed_query):
+    weight_query_vector = []
+    for term in vocabulary:
+        weight_query_vector.append(weight_for_query(term, processed_query))
+    return weight_query_vector
