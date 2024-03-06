4af63f751e5bf8168b2c5dd0c7d3ca31dd5eb030
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-03-05 22:12:19 -0500

    documentado preprocessing_query

--- src/code/preprocessing_query.py
+++ src/code/preprocessing_query.py
@@ -1,6 +1,16 @@
 from preprocessing_functions import *
 
 def preprocess_query(query):
+    """
+    Preprocess a query by tokenizing, cleaning, removing stopwords, reducing morphological forms,
+    filtering tokens by occurrence, and building a vocabulary.
+
+    Args:
+        query (str): The input query to be preprocessed.
+
+    Returns:
+        list: A list of tokens representing the vocabulary after preprocessing the query.
+    """
     tokenized_query = tokenization([query])
     cleaned_query = remove_noise(tokenized_query)
     no_stop_words_query = remove_stopwords(cleaned_query)
